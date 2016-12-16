d703cf602b67371b48fb5d308d6420111bee22a3
Logan Katz <loganzk@gmail.com>
2016-12-16 12:10:11 -0800

    Day2

--- 2016/src/day2.py
+++ 2016/src/day2.py
@@ -2,7 +2,7 @@ class Keypad(object):
 
     def __init__(self, key_layout):
         self.dirs = {'U':0, 'R':1, 'D':2, 'L':3}
-        self.last_digit = 5
+        self.last_digit = '5'
         self.code = []
         self.keypad = self.parse_keypad(key_layout)
     
@@ -10,8 +10,32 @@ class Keypad(object):
         #layout  = {1: (1,2,4,1), 2: (2,3,5,1), 3: (3,3,6,2),
         #               4: (1,5,7,4), 5: (2,6,8,4), 6: (3,6,9,5),
         #               7: (4,8,7,7), 8: (5,9,8,7), 9: (6,9,9,8)} 
+        key_dict = {}
+        k = kp.split('\n')
+        k.pop(-1)
+        for i, line in enumerate(k):
+            for j, char in enumerate(line):
+                if char == ' ':
+                    continue
+                else:
+                    key_dict[char] = self.get_neighbors(k, i, j)
+
+        return key_dict 
+
+    def get_neighbors(self, kp, i, j):
+        dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        out = []
+        for v, h in dirs:
+            tempx, tempy = v + i, h + j 
+            xidx = i if tempx < 0 or tempx >= len(kp) else tempx 
+            yidx = j if tempy < 0 or tempy >= len(kp[0]) else tempy
+            char = kp[xidx][yidx]
+            if char == ' ':
+                out.append(kp[i][j])
+            else:
+                out.append(char)
+        return tuple(out)
         
-        return layout
 
     def find_code(self, inst):
         for sequence in inst:
@@ -32,12 +56,12 @@ def read_instructions(fp):
 
 def main(inst):
     #Part 1
-    kp1 = '1 2 3\n4 5 6\n7 8 9\n'
+    kp1 = '123\n456\n789\n'
     kp = Keypad(kp1)
     print kp.find_code(inst)
     
     #Part 2
-    kp2 = '   1    \n  2 3 4  \n5 6 7 8 9\n  A B C  \n    D    \n'  
+    kp2 = '  1  \n 234 \n56789\n ABC \n  D  \n'  
     kp = Keypad(kp2)
     print kp.find_code(inst)
 
