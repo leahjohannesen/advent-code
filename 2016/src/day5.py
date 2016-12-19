fa4c5612b087bef4acd884cf47f362975137ba50
Logan Katz <loganzk@gmail.com>
2016-12-19 15:40:09 -0800

    Added day5

--- 2016/src/day5.py
+++ 2016/src/day5.py
@@ -1,10 +1,28 @@
-def read_instructions(fp):
-    with open(fp, 'r') as f:
-        return f.readlines()
+import hashlib
+
+def find_hash(key):
+    key_len = 0
+    idx = 0
+    pw = [None]*8
+    while key_len < 8:
+        dig = hashlib.md5(key + str(idx)).hexdigest()        
+        if dig[:5] == '00000':
+            try:
+                loc = int(dig[5])
+                if not pw[loc]:
+                    pw[loc] = dig[6]
+                    key_len += 1
+            except:
+                pass
+        idx += 1
+    return pw
+
 def main():
     #Part 1
-    toy1 = 'abc'
-    inst = read_instructions('input/day4.txt')
+    toy = 'abc'
+    inst = 'ffykfhsq'
+    print find_hash(toy)
+    print find_hash(inst)
 
 if __name__=='__main__':
     main()
